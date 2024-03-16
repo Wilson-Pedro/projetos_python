64cb53c1e57c7110fca84d8e95532ea58745d22a
Wilson-Pedro <wilsonpedroneto@gmail.com>
2024-03-16 18:58:08 -0300

    feat: alterações no homepage

--- Boletim/view/siteEscolaLuluzinha.py
+++ Boletim/view/siteEscolaLuluzinha.py
@@ -5,8 +5,8 @@ import requests
 app = Flask(__name__)
 
 app.config['MYSQL_HOST'] = '127.0.0.1'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'admin1'
+app.config['MYSQL_USER'] = 'dveloper'
+app.config['MYSQL_PASSWORD'] = '1234567'
 app.config['MYSQL_DB'] = 'escola'
 
 mysql = MySQL(app)
